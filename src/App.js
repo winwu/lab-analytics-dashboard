10fe9361efdffce5dbb0036191ec04f50f205159
winwu <look_me08@hotmail.com>
2020-01-30 22:33:21 +0800

    add react router and demo for router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+
+// import logo from './logo.svg';
 import './App.css';
+
+function Dashboard() {
+    return <h2>Dashboard</h2>;
+}
+  
+function Users() {
+    return <h2>Users</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="/users">
+                    <Users />
+                </Route>
+                <Route path='/'>
+                    <Redirect to="/dashboard" />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
